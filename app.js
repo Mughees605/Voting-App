7ca56486446f22feefe06705dffdae4ce3f967b6
mughees605 <mughees605@gmail.com>
2017-12-25 17:22:45 +0500

    minor change

--- app.js
+++ app.js
@@ -48,8 +48,8 @@ app.use('/polls', polls)
 // Index Route
 
 // Catch all other routes and return the index file
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 })
 
 // Start Server
